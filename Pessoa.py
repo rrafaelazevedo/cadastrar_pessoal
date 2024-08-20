6a0aec545313af9afecdead8101b99af6fd9d8cf
RAFAEL AZEVEDO DE SOUZA <47624259826@info.local>
2024-08-19 22:12:13 -0300

    commit_cadastro

---
+++ Pessoa.py
@@ -0,0 +1,55 @@
+from datetime import date
+
+# CLASSE ENDEREÇO
+class Endereco:
+    def __init__(self, logradouro='', numero='', endereco_comercial=False):
+        self.logradouro = logradouro
+        self.numero = numero
+        self.endereco_comercial = endereco_comercial
+
+# CLASSE PESSOA
+class Pessoa:
+    def __init__(self, nome='', rendimento=0.0, endereco=None):
+        self.nome = nome
+        self.rendimento = rendimento
+        self.endereco = endereco if endereco is not None else Endereco()
+
+    def calcular_imposto(self):
+        return self.rendimento
+
+# CLASSE PESSOA FÍSICA
+class PessoaFisica(Pessoa):
+    def __init__(self, nome='', rendimento=0.0, endereco=None, cpf='', data_nascimento=None):
+        if endereco is None:
+            endereco = Endereco()
+        
+        if data_nascimento is None:
+            data_nascimento = date.today()
+
+        super().__init__(nome, rendimento, endereco)
+        self.cpf = cpf
+        self.data_nascimento = data_nascimento
+
+    def calcular_imposto(self, rendimento: float) -> float:
+        # sem imposto para rendimentos até 1500
+        if self.rendimento <= 1500:
+            return 0
+        # 2% de imposto para rendimento entre 1500 e 3500
+        elif 1500 < self.rendimento <= 3500:
+            return (self.rendimento - 1500) * 0.02
+        # 3.5% de imposto para rendimentos entre 3500 e 6000
+        elif 3500 < self.rendimento <= 6000:
+            return (self.rendimento - 3500) * 0.035 + (3500 - 1500) * 0.02
+        # 5% de imposto para rendimentos acima de 6000
+        else:
+            return (self.rendimento - 6000) * 0.05 + (6000 - 3500) * 0.035 + (3500 - 1500) * 0.02
+
+# CLASSE PESSOA JURÍDICA
+class PessoaJuridica(Pessoa):
+    def __init__(self, nome='', rendimento=0.0, endereco=None, cnpj=''):
+        super().__init__(nome, rendimento, endereco)
+        self.cnpj = cnpj
+
+    def calcular_imposto(self) -> float:
+        # Exemplo básico: 10% de imposto para Pessoa Jurídica
+        return self.rendimento * 0.10
---
+++ Sistema.py
@@ -0,0 +1,78 @@
+from Pessoa import Endereco, PessoaFisica
+from datetime import date, datetime
+
+# lista global para armazenar pessoas físicas
+lista_pf = []
+
+def main():
+    while True:
+        opcao = int(input('Digite uma opção: 1 - Pessoa Física | 2 - Pessoa Juridica | 0 - Sair '))
+
+        if opcao == 1:
+            while True:
+                opcao_pf = int(input('Digite uma opção: 1 - Cadastrar pessoa física | 2 - Listar pessoa física | 0 - Voltar ao menu anterior '))
+                
+                # cadastrar uma pessoa física
+                if opcao_pf == 1:
+                    nova_pf = PessoaFisica()
+                    novo_end_pf = Endereco()
+                    nova_pf.nome = input('Digite o nome da pessoa física: ')
+                    nova_pf.cpf = input('Digite o CPF: ')
+                    nova_pf.rendimento = float(input('Digite o rendimento mensal (somente números): '))
+
+                    data_nascimento = input('Digite a data de nascimento (dd/MM/yyyy): ')  # solicita data de nascimento
+                    nova_pf.data_nascimento = datetime.strptime(data_nascimento, '%d/%m/%Y').date()
+                    idade = (date.today() - nova_pf.data_nascimento).days // 365  #calcula a idade da pessoa
+
+                    if idade >= 18:
+                        print('A pessoa tem mais de 18 anos')
+                    else:
+                        print('A pessoa tem menos de 18 anos. Retornando ao menu...')
+                        continue  # retorna ao início do looping
+
+                    # cadastro de endereço
+                    novo_end_pf.logradouro = input('Digite o logradouro: ')
+                    novo_end_pf.numero = input('Digite um número: ')
+                    end_comercial = input('Este endereço é comercial? (S/N): ')
+                    novo_end_pf.endereco_comercial = end_comercial.strip().upper() == 'S'
+                    nova_pf.endereco = novo_end_pf
+
+                    lista_pf.append(nova_pf)
+                    
+                    print('Cadastro realizado com sucesso!')
+
+                # listar pessoas físicas
+                elif opcao_pf == 2:
+                    if lista_pf:
+                        for cada_pf in lista_pf:
+                            print(f'\nNome: {cada_pf.nome}')
+                            print(f'CPF: {cada_pf.cpf}')
+                            print(f'Endereço: {cada_pf.endereco.logradouro}, {cada_pf.endereco.numero}')
+                            print(f'Data de Nascimento: {cada_pf.data_nascimento.strftime("%d/%m/%Y")}')
+                            print(f'Imposto a ser pago: {cada_pf.calcular_imposto()}')
+                            print('Digite qualquer tecla para continuar...')
+                            input()
+                    else:
+                        print('LISTA VAZIA []')
+                
+                # sair do menu atual
+                elif opcao_pf == 0:
+                    print('Retornando ao menu anterior')
+                    break
+                
+                else:
+                    print('Opção inválida, por favor digite uma das opções indicadas.')
+
+        elif opcao == 2:
+            print('Funcionalidade para pessoa jurídica não implementada.')
+            pass
+
+        elif opcao == 0:
+            print('Obrigado por utilizar o nosso sistema!')
+            break
+    
+        else:
+            print('Opção inválida! Por favor, digite uma das opções válidas.')
+
+if __name__ == '__main__':
+    main()  # chama a função principal
